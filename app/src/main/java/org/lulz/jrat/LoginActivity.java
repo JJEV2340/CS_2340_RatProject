dcb64310fa335da4aab4a7326468227d911daa3c
vksrinivasan <ndnkai@gmail.com>
2017-09-24 20:50:22 -0400

    Revert "deleted app" undelet This reverts commit 0cbf1df5950cc7681ecb175e15958100edab5c9c.

---
+++ app/src/main/java/org/lulz/jrat/LoginActivity.java
@@ -0,0 +1,98 @@
+package org.lulz.jrat;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.util.Log;
+import org.lulz.jrat.jrat.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // Start up my activity login view
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Create a variable to store reference the button
+        Button loginButton = null;
+
+        loginButton = (Button) findViewById(R.id.email_sign_in_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        // Create another button variable for registration
+        Button registerButton = null;
+
+        registerButton = (Button) findViewById(R.id.registrationButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent regIntent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(regIntent);
+            }
+        });
+    }
+
+    public void login() {
+        Log.d(TAG, "Login Attempt");
+        if(!validate()) {
+            ;
+        }
+        else {
+            Intent intent = new Intent(getApplicationContext(), AppHome.class);
+            startActivity(intent);
+        }
+    }
+
+    public boolean validate() {
+        String username = ((AutoCompleteTextView) findViewById(R.id.username)).getText().toString();
+        String password = ((EditText) findViewById(R.id.password)).getText().toString();
+
+
+        // Eventually we can implement more sophisticated ideas here for
+        // authentication and security
+        if(username.equals("Rat") && password.equals("Test")) {
+            return true;
+        }
+        return false;
+    }
+}
