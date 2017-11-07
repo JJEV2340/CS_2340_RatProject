a372b023454b3137ed1bcbbfd74a4edf94c981b3
Jin Woo Lee <excalivur@naver.com>
2017-11-07 14:14:40 -0500

    added methods to be tested

--- app/src/main/java/org/lulz/jrat/Util.java
+++ app/src/main/java/org/lulz/jrat/Util.java
@@ -1,4 +1,73 @@
 package org.lulz.jrat;
 
+
+/**
+ * Util class that checks for ID, PW, email validity
+ */
 public class Util {
+
+    private static final char[] symbols = {'~', '`', '@', '!', '#', '$', '%',
+            '^', '&', '*', '*', '(', ')', '-', '=', '+', '[', ']', '{', '}',
+            ',', '.', ';', ' ', '"', ':', '<', '>', '/', '?', '|', '_'};
+
+    /**
+     * check for validity of email address
+     * @param username username to be checked
+     * @return true if valid
+     */
+    public boolean isuUsernameValid(String username) {
+
+        boolean validity = true;
+
+        // check if the username contains any special symbol
+        for (int i = 0; i < symbols.length; i++) {
+            if (username.contains(Character.toString(symbols[i])) ) {
+                validity = false;
+            }
+        }
+
+        if (username.length() < 5 || username.length() > 10) {
+            validity = false;
+        }
+        return validity;
+    }
+
+    /**
+     * check for validity of password
+     * @param password password user entered during registration
+     * @return true if valid
+     */
+    public boolean isPasswordValid(String password) {
+        boolean validity = true;
+
+        // check if the password contains any special symbol
+        for (int i = 0; i < symbols.length; i++) {
+            if (password.contains(Character.toString(symbols[i])) ) {
+                validity = false;
+            }
+        }
+
+        if (password.length() < 5
+                || password.length() > 10) {
+            validity = false;
+        }
+
+        if (password.equals("1234")) {
+            validity = false;
+        }
+        return validity;
+    }
+
+    /**
+     * check for validity of email address
+     * @param email email user entered during registration
+     * @return true if valid
+     */
+    public boolean isEmailValid(String email) {
+        boolean validity = true;
+        if (!email.contains("@") || email.contains(" ")) {
+            validity = false;
+        }
+        return validity;
+    }
 }
